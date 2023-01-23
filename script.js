011db5eaf112cfa0691d7a3f3aa574f0443c1c30
Snehashish-Ghosh <necshuvo@gmail.com>
2023-01-23 20:44:49 +0600

    all done

---
+++ script.js
@@ -0,0 +1,39 @@
+// Eamil JS
+const submitButton = document.getElementById("submitButton");
+
+submitButton.addEventListener("click", function () {
+  const nameField = document.getElementById("contactName");
+  const emailField = document.getElementById("contactEmail");
+  const messageField = document.getElementById("contactMessage");
+  const errorText = document.getElementById("error-text");
+
+  const nameValue = nameField.value;
+  const emailValue = emailField.value;
+  const messageValue = messageField.value;
+
+  const templateParams = {
+    nameValue: nameValue,
+    emailValue: emailValue,
+    messageValue: messageValue,
+  };
+
+  if (nameValue && emailValue && messageValue) {
+    console.log(nameValue, emailValue, messageValue);
+    emailjs.send(
+      "service_class28test",
+      "template_plmfniq",
+      templateParams,
+      "uptWrWYIZaMbXfJXw"
+    );
+  }
+});
+
+// Preloader Function
+window.onload = function () {
+  const Preloader = document.getElementById("Preloader-area");
+  Preloader.className = "page-loaded";
+
+  setTimeout(function () {
+    Preloader.style.display = "none";
+  }, 500);
+};
